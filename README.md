818ed326f4b3cfff0cb183543d670b762b89bb8b
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-04-20 11:34:53 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+<p align="center">
+  <img src="./w3tec-logo.png" alt="w3tec" width="400" />
+</p>
+
+<h1 align="center">Reveal.js Boilerplate</h1>
+
+<p align="center">
+  <b>A simple and comfortable way to make a presentation</b></br>
+</br>
+  <sub>Made with ❤️ by <a href="https://github.com/w3tecch">w3tech</a> & <a href="https://github.com/w3tecch/reveal.js-boilerplate/graphs/contributors">contributors</a></sub>
+</p>
+
+<br />
+
+## ❯ Why
+
+Full configured [reveal.js](https://revealjs.com/) sample project with [webpack](https://webpack.js.org/) and some awesome features:
+
+### Features
+
+- **Markdown & HTML Support** Write your slides with the markdown or html language.
+- **Sass** Define your styles with [Sass](https://sass-lang.com/).
+- **Github Deployment** Run `npm start deploy` to deploy your builded presentation on th github pages.
+
+![divider](./w3tec-divider.png)
+
+## ❯ Table of Contents
+
+- [Getting Started](#-getting-started)
+- [Scripts and Tasks](#-scripts-and-tasks)
+- [License](#-license)
+
+![divider](./w3tec-divider.png)
+
+## ❯ Getting Started
+
+### Step 1: Set up the Development Environment
+
+You need to set up your development environment before you can do anything.
+
+Install [Node.js and NPM](https://nodejs.org/en/download/)
+
+- on OSX use [homebrew](http://brew.sh) `brew install node`
+- on Windows use [chocolatey](https://chocolatey.org/) `choco install nodejs`
+
+Install yarn globally
+
+```bash
+npm install yarn -g
+```
+
+### Step 2: Serve your App
+
+Go to the project dir and start your app with this npm script.
+
+```bash
+npm start
+```
+
+![divider](./w3tec-divider.png)
+
+## ❯ Scripts and Tasks
+
+All script are defined in the `package-scripts.js` file, but the most important ones are listed here.
+
+### Install
+
+- Install all dependencies with `yarn install`
+
+### Serve
+
+- Run `npm start` to serve your presentation locally
+
+### Deploy
+
+- Run `npm start deploy` to deploy your presentation on the github pages.
+
+![divider](./w3tec-divider.png)
+
+## ❯ License
+
+[MIT](/LICENSE)
---
+++ src/app.js
@@ -0,0 +1,82 @@
+import './sass/main.scss';
+
+import 'babel-polyfill';
+import $ from 'jquery';
+import Reveal from 'reveal.js';
+import hljs from 'highlight.js';
+import { RevealMarkdown } from 'reveal.js/plugin/markdown/markdown';
+
+// Fontawesome setup
+import fontawesome from '@fortawesome/fontawesome';
+import fontawesomeSolid from '@fortawesome/fontawesome-free-solid';
+fontawesome.library.add(fontawesomeSolid);
+
+// Star Wars Theme
+import swLead from './slides/000-sw-lead.slide.html';
+import swTitle from './slides/001-sw-title.slide.html';
+
+import pageAuthor from './slides/100-author.slide.html';
+import pageWhy from './slides/101-why.slide.html';
+import pageExpressHelloWorld from './slides/110-express-hello-world.slide.html';
+import pageExpressDI from './slides/111-express-di.slide.html';
+import pageExpressAnnotation from './slides/112-express-annotation.slide.html';
+import pageFeatures from './slides/120-features.slide.html';
+import pageBigPicture from './slides/130-big-picture.slide.html';
+import pageArchitecure from './slides/131-architecure.slide.html';
+import pageStack from './slides/132-stack.slide.html';
+import pagePersistence from './slides/140-persistence.html';
+import pageService from './slides/141-service.html';
+import pageGetStated0 from './slides/150-get-started.html';
+import pageGetStated1 from './slides/151-get-started.html';
+import pageGetStated2 from './slides/152-get-started.html';
+import pageLetsCode from './slides/160-lets-code.html';
+
+// -------------------------------------------------------------------------
+// Slides
+// -------------------------------------------------------------------------
+
+const slides = [
+  swLead,
+  swTitle,
+
+  pageAuthor,
+  pageWhy,
+
+  pageExpressHelloWorld,
+  pageExpressDI,
+  pageExpressAnnotation,
+  pageFeatures,
+
+  pageBigPicture,
+  pageArchitecure,
+  pageStack,
+  pagePersistence,
+  pageService,
+
+  pageGetStated0,
+  pageGetStated1,
+  pageGetStated2,
+  pageLetsCode
+]
+  .forEach(s => $('#slides').append(s));
+
+// -------------------------------------------------------------------------
+// Reveal.js Configuration
+// -------------------------------------------------------------------------
+
+Reveal.initialize({
+  center: false,
+  margin: 0,
+  width: "100%",
+  height: "100%",
+});
+
+RevealMarkdown.initialize();
+
+hljs.initHighlightingOnLoad();
+
+/*
+
+RESTful applications use HTTP requests to perform four operations termed as CRUD (C: create, R: read, U: update, and D: delete). Create and/or update is used to post data, get for reading/listing data, and delete to remove data.
+
+*/
